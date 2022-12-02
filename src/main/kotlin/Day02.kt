5026127245951cf7ab13ebb27a34eb4faa925a9b
Matthew Wilson <mfwilson1@gmail.com>
2022-12-01 23:31:27 -0800

    hardcoded score map instead of nested when

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -3,70 +3,23 @@ import java.io.File
 fun main(args: Array<String>) {
     println("2022 Advent of Code day 2")
 
-    val guide = ArrayList<Pair<String, String>>()
-
-    File("day2input").forEachLine { line ->
-        line.split(" ").also {
-           guide.add(Pair(it[0], it[1]))
-        }
-    }
+    // Setup - read the "strategy guide"
+    val guide = File("day2input").readLines()
     println("There are ${guide.size} entries in the strategy guide")
 
     // Part 1 - play the game according to the guide and calculate the score
-    var score = 0
-
-    guide.forEach { entry ->
-        when (entry.first) {
-            "A" -> {
-                when (entry.second) {
-                    "X" -> score += 4
-                    "Y" -> score += 8
-                    "Z" -> score += 3
-                }
-            }
-            "B" -> {
-                when (entry.second) {
-                    "X" -> score += 1
-                    "Y" -> score += 5
-                    "Z" -> score += 9
-                }
-            }
-            "C" -> {
-                when (entry.second) {
-                    "X" -> score += 7
-                    "Y" -> score += 2
-                    "Z" -> score += 6
-                }
-            }
-        }
-    }
-    println("Part 1 - after following the guide, the final score is $score")
+    val scoreMap1 = mapOf(
+        "A X" to 4, "A Y" to 8, "A Z" to 3,
+        "B X" to 1, "B Y" to 5, "B Z" to 9,
+        "C X" to 7, "C Y" to 2, "C Z" to 6,
+    )
+    println("Part 1 - after following the guide, the final score is ${guide.sumOf { scoreMap1[it]!! }}")
 
     // Part 2 - play the game with the new interpretation of the guide and calculate the score
-    score = 0
-    guide.forEach { entry ->
-        when (entry.first) {
-            "A" -> {
-                when (entry.second) {
-                    "X" -> score += 3
-                    "Y" -> score += 4
-                    "Z" -> score += 8
-                }
-            }
-            "B" -> {
-                when (entry.second) {
-                    "X" -> score += 1
-                    "Y" -> score += 5
-                    "Z" -> score += 9
-                }
-            }
-            "C" -> {
-                when (entry.second) {
-                    "X" -> score += 2
-                    "Y" -> score += 6
-                    "Z" -> score += 7
-                }
-            }
-        }
-    }
-    println("Part 2 - after following the guide, the final score is $score")}
+    val scoreMap2 = mapOf(
+        "A X" to 3, "A Y" to 4, "A Z" to 8,
+        "B X" to 1, "B Y" to 5, "B Z" to 9,
+        "C X" to 2, "C Y" to 6, "C Z" to 7,
+    )
+    println("Part 2 - after following the guide, the final score is ${guide.sumOf { scoreMap2[it]!! }}")
+}
