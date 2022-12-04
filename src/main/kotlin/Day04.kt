6e50d1f8467f67c305f64b08e2149654692b8047
Matthew Wilson <mfwilson1@gmail.com>
2022-12-03 22:10:52 -0800

    less efficient but more concise

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -6,18 +6,13 @@ fun main(args: Array<String>) {
     // Setup - read the assignment details
     val assignments = File("day4input").readLines().map { line ->
         line.split(",").map { range ->
-            range.split("-").let {
-                IntRange(it[0].toInt(), it[1].toInt())
-            }
+            range.split("-").let { IntRange(it[0].toInt(), it[1].toInt()).toList() }
         }.let { Pair(it[0], it[1]) }
     }
     println("There are ${assignments.size} assignments to check")
 
     // Part 1 - find the fully overlapping assignments
-    val part1 = assignments.count {
-        (it.first.first >= it.second.first && it.first.last <= it.second.last) ||
-        (it.second.first >= it.first.first && it.second.last <= it.first.last)
-    }
+    val part1 = assignments.count { it.first.containsAll(it.second) || it.second.containsAll(it.first) }
     println("There are $part1 fully overlapping assignments")
 
     // Part 2 - find the partially overlapping assignments
