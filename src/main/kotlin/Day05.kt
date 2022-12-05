e2a71b099a0de07c46c1ab00bfdf692ac7317ded
Matthew Wilson <mfwilson1@gmail.com>
2022-12-04 23:18:46 -0800

    refactor names

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -8,12 +8,12 @@ fun main(args: Array<String>) {
 
     // Parse the starting crate configuration
     val cratesInput = input.first().split("\n").reversed()
-    val numCrates = cratesInput.first().trim().split(" ").last().toInt()
-    val crates = ArrayList<MutableList<Char>>(numCrates)
-    repeat(numCrates) { i -> crates.add(mutableListOf()) }
+    val numStacks = cratesInput.first().trim().split(" ").last().toInt()
+    val initialStacks = ArrayList<ArrayList<Char>>(numStacks)
+    repeat(numStacks) { i -> initialStacks.add(arrayListOf()) }
     cratesInput.drop(1).forEach { line ->
         line.drop(1).windowed(1, 4).forEachIndexed { i, item ->
-            if (item.isNotBlank()) crates[i].add(0, item.first())
+            if (item.isNotBlank()) initialStacks[i].add(0, item.first())
         }
     }
 
@@ -26,20 +26,22 @@ fun main(args: Array<String>) {
         }
     }
 
-    // Part 1 - follow the instructions and get the top crates
+    // Part 1 - follow the instructions moving one crate at a time
+    val stacks1 = initialStacks.map { ArrayList(it) }
     instructions.forEach { inst ->
         repeat(inst.first) {
-            crates[inst.third - 1].add(0, crates[inst.second - 1].removeFirst())
+            stacks1[inst.third - 1].add(0, stacks1[inst.second - 1].removeFirst())
         }
     }
-    val part1 = crates.map { it.first() }.joinToString("")
-    println(part1)
+    val part1 = stacks1.map { it.first() }.joinToString("")
+    println("The items on top of each stack after part1 are $part1")
 
-    // Part 2 - follow the instructions and get the top crates
+    // Part 2 - follow the instructions moving all crates at a time
+    val stacks2 = ArrayList(initialStacks.map { ArrayList(it) })
     instructions.forEach { inst ->
-        crates[inst.third - 1].addAll(0, crates[inst.second - 1].take(inst.first))
-        crates[inst.second - 1] = crates[inst.second - 1].drop(inst.first).toMutableList()
+        stacks2[inst.third - 1].addAll(0, stacks2[inst.second - 1].take(inst.first))
+        stacks2[inst.second - 1] = ArrayList(stacks2[inst.second - 1].drop(inst.first))
     }
-    val part2 = crates.map { it.firstOrNull() }.joinToString("")
-    println(part2)
+    val part2 = stacks2.map { it.firstOrNull() }.joinToString("")
+    println("The items on top of each stack after part2 are $part2")
 }
