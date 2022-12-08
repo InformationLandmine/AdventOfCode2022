31ec2128d2b0cdb889b65e0fd61ae91cd4181698
Matthew Wilson <mfwilson1@gmail.com>
2022-12-08 10:51:26 -0800

    clean up

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -7,60 +7,48 @@ fun main(args: Array<String>) {
     val map = File("day8input").readLines().map { line -> line.map { it.digitToInt() } }
 
     // Part 1 - get the visible tree count
-    var visibleCount = 0
-    for (row in 0..map.lastIndex) {
-        for (col in 0..map[row].lastIndex) {
-            visibleCount += when (isVisible(map, Pair(row, col))) { true -> 1 false -> 0}
-        }
-    }
-    println(visibleCount)
+    val part1 = map.flatMapIndexed { r, row -> row.mapIndexed { c, h -> isVisible(map, r, c, h) } }.count { it }
+    println(part1)
 
     // Part 2 - get the max scenic score
-    val scenicScores = ArrayList<Int>()
-    for (row in 0..map.lastIndex) {
-        for (col in 0..map[row].lastIndex) {
-            scenicScores.add(scenicScore(map, Pair(row, col)))
-        }
-    }
-    println(scenicScores.max())
+    val part2 = map.flatMapIndexed { r, row -> row.mapIndexed { c, h -> scenicScore(map, r, c, h) } }.max()
+    println(part2)
 }
 
-fun isVisible(map: List<List<Int>>, point: Pair<Int, Int>): Boolean {
-    val height = map[point.first][point.second]
+fun isVisible(map: List<List<Int>>, row: Int, col: Int, height: Int): Boolean {
     // Check up
-    val up = !(0 until point.first).any { map[it][point.second] >= height }
+    val up = !(0 until row).any { map[it][col] >= height }
     // Check down
-    val down = !(point.first + 1..map.lastIndex).any { map[it][point.second] >= height }
+    val down = !(row + 1..map.lastIndex).any { map[it][col] >= height }
     // Check left
-    val left = !(0 until point.second).any { map[point.first][it] >= height }
+    val left = !(0 until col).any { map[row][it] >= height }
     // Check right
-    val right = !(point.second + 1..map[0].lastIndex).any { map[point.first][it] >= height }
+    val right = !(col + 1..map[0].lastIndex).any { map[row][it] >= height }
 
     return up || down || left || right
 }
 
-fun scenicScore(map: List<List<Int>>, point: Pair<Int, Int>): Int {
-    val height = map[point.first][point.second]
+fun scenicScore(map: List<List<Int>>, row: Int, col: Int, height: Int): Int {
     // Check up
-    var up = (point.first - 1 downTo 0).firstOrNull { map[it][point.second] >= height }?.let {
-        point.first - it
+    var up = (row - 1 downTo 0).firstOrNull { map[it][col] >= height }?.let {
+        row - it
     }
-    if (up == null) up = point.first
+    if (up == null) up = row
     // Check down
-    var down = (point.first + 1..map.lastIndex).firstOrNull { map[it][point.second] >= height }?.let {
-        it - point.first
+    var down = (row + 1..map.lastIndex).firstOrNull { map[it][col] >= height }?.let {
+        it - row
     }
-    if (down == null) down = map.lastIndex - point.first
+    if (down == null) down = map.lastIndex - row
     // Check left
-    var left = (point.second - 1 downTo  0).firstOrNull { map[point.first][it] >= height }?.let {
-        point.second - it
+    var left = (col - 1 downTo  0).firstOrNull { map[row][it] >= height }?.let {
+        col - it
     }
-    if (left == null) left = point.second
+    if (left == null) left = col
     // Check right
-    var right = (point.second + 1..map[0].lastIndex).firstOrNull { map[point.first][it] >= height }?.let {
-        it - point.second
+    var right = (col + 1..map[0].lastIndex).firstOrNull { map[row][it] >= height }?.let {
+        it - col
     }
-    if (right == null) right = map[0].lastIndex - point.second
+    if (right == null) right = map[0].lastIndex - col
 
     return up * down * left * right
 }
