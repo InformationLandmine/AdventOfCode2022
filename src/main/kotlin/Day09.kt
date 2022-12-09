fd2a8471f4962f8b75fc3fce42511eb051cc8d07
Matthew Wilson <mfwilson1@gmail.com>
2022-12-09 11:10:17 -0800

    implement generic move closer

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import kotlin.math.abs
 
 fun main(args: Array<String>) {
     println("2022 Advent of Code day 9")
@@ -10,128 +11,39 @@ fun main(args: Array<String>) {
     val tail = RopeKnot(null)
     var head = RopeKnot(tail)
     // Add 8 more knots for part 2
-    // repeat(8) {
-    //     head = RopeKnot(head)
-    // }
+    repeat(8) {
+        head = RopeKnot(head)
+    }
     val visited: MutableSet<Point> = mutableSetOf(tail.location)
     input.forEach { instruction ->
-        val operation = when (instruction.first) {
-            "L" -> ::moveLeft
-            "R" -> ::moveRight
-            "U" -> ::moveUp
-            "D" -> ::moveDown
+        val offset = when (instruction.first) {
+            "L" -> Point(-1, 0)
+            "R" -> Point(1, 0)
+            "U" -> Point(0, 1)
+            "D" -> Point(0, -1)
             else -> throw Exception("Invalid input direction")
         }
         repeat(instruction.second) {
-            operation(head)
+            move(head, offset)
             visited.add(tail.location)
         }
     }
     println("The number of positions the tail visits at least once is ${visited.size}")
 }
 
-fun moveLeft(knot: RopeKnot) {
-    knot.location = Point(knot.location.x - 1, knot.location.y)
+fun move(knot: RopeKnot, offset: Point) {
+    knot.location += offset
     knot.next?.let {
         if (!knot.location.surrounding.contains(it.location)) {
-            if (it.location.y > knot.location.y) moveDownLeft(it)
-            else if (it.location.y < knot.location.y) moveUpLeft(it)
-            else moveLeft(it)
+            move(it, moveCloser(it, knot.location))
         }
     }
 }
 
-fun moveUpLeft(knot: RopeKnot) {
-    knot.location = Point(knot.location.x - 1, knot.location.y + 1)
-    knot.next?.let {
-        if (!knot.location.surrounding.contains(it.location)) {
-            if (it.location.y == knot.location.y) moveLeft(it)
-            else if (it.location.y == knot.location.y - 1) moveUpLeft(it)
-            else if (it.location.y == knot.location.y - 2) {
-                if (it.location.x == knot.location.x) moveUp(it)
-                else moveUpLeft(it)
-            }
-            else throw Exception("Error moving up left")
-        }
-    }
-}
-
-fun moveUp(knot: RopeKnot) {
-    knot.location = Point(knot.location.x, knot.location.y + 1)
-    knot.next?.let {
-        if (!knot.location.surrounding.contains(it.location)) {
-            if (it.location.x > knot.location.x) moveUpLeft(it)
-            else if (it.location.x < knot.location.x) moveUpRight(it)
-            else moveUp(it)
-        }
-    }
-}
-
-fun moveUpRight(knot: RopeKnot) {
-    knot.location = Point(knot.location.x + 1, knot.location.y + 1)
-    knot.next?.let {
-        if (!knot.location.surrounding.contains(it.location)) {
-            if (it.location.y == knot.location.y) moveRight(it)
-            else if (it.location.y == knot.location.y - 1) moveUpRight(it)
-            else if (it.location.y == knot.location.y - 2) {
-                if (it.location.x == knot.location.x) moveUp(it)
-                else moveUpRight(it)
-            }
-            else throw Exception("Error moving up right")
-        }
-    }
-}
-
-fun moveRight(knot: RopeKnot) {
-    knot.location = Point(knot.location.x + 1, knot.location.y)
-    knot.next?.let {
-        if (!knot.location.surrounding.contains(it.location)) {
-            if (it.location.y > knot.location.y) moveDownRight(it)
-            else if (it.location.y < knot.location.y) moveUpRight(it)
-            else moveRight(it)
-        }
-    }
-}
-
-fun moveDownRight(knot: RopeKnot) {
-    knot.location = Point(knot.location.x + 1, knot.location.y - 1)
-    knot.next?.let {
-        if (!knot.location.surrounding.contains(it.location)) {
-            if (it.location.y == knot.location.y) moveRight(it)
-            else if (it.location.y == knot.location.y + 1) moveDownRight(it)
-            else if (it.location.y == knot.location.y + 2) {
-                if (it.location.x == knot.location.x) moveDown(it)
-                else moveDownRight(it)
-            }
-            else throw Exception("Error moving down right")
-        }
-    }
-}
-
-fun moveDown(knot: RopeKnot) {
-    knot.location = Point(knot.location.x, knot.location.y - 1)
-    knot.next?.let {
-        if (!knot.location.surrounding.contains(it.location)) {
-            if (it.location.x > knot.location.x) moveDownLeft(it)
-            else if (it.location.x < knot.location.x) moveDownRight(it)
-            else moveDown(it)
-        }
-    }
-}
-
-fun moveDownLeft(knot: RopeKnot) {
-    knot.location = Point(knot.location.x - 1, knot.location.y - 1)
-    knot.next?.let {
-        if (!knot.location.surrounding.contains(it.location)) {
-            if (it.location.y == knot.location.y) moveLeft(it)
-            else if (it.location.y == knot.location.y + 1) moveDownLeft(it)
-            else if (it.location.y == knot.location.y + 2) {
-                if (it.location.x == knot.location.x) moveDown(it)
-                else moveDownLeft(it)
-            }
-            else throw Exception("Error moving down left")
-        }
-    }
+fun moveCloser(knot: RopeKnot, loc: Point): Point {
+    val dx = if (knot.location.x == loc.x) 0 else -1 * abs(knot.location.x - loc.x) / (knot.location.x - loc.x)
+    val dy = if (knot.location.y == loc.y) 0 else -1 * abs(knot.location.y - loc.y) / (knot.location.y - loc.y)
+    return Point(dx, dy)
 }
 
 data class Point(val x:Int, val y:Int) {
@@ -141,6 +53,9 @@ data class Point(val x:Int, val y:Int) {
             Point(x - 1, y), Point(x, y), Point(x + 1, y),
             Point(x - 1, y - 1), Point(x, y - 1), Point(x + 1, y - 1),
         )
+    }
+    operator fun plus(other: Point): Point {
+        return Point(this.x + other.x, this.y + other.y)
     }
 }
 
