5ae36176c527c6eca58aea56e22ca7fd9c44f323
Matthew Wilson <mfwilson1@gmail.com>
2022-12-22 14:49:09 -0800

    added heuristic to make part 2 reasonable

--- src/main/kotlin/Day19.kt
+++ src/main/kotlin/Day19.kt
@@ -1,11 +1,12 @@
 import java.io.File
+import kotlin.math.max
 
 fun main(args: Array<String>) {
     println("2022 Advent of Code day 19")
 
     // Setup - read the blueprints
     val blueprintRegex = """Blueprint (\d+): Each ore robot costs (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian robot costs (\d+) ore and (\d+) clay. Each geode robot costs (\d+) ore and (\d+) obsidian.""".toRegex()
-    val factories = File("day19test").readLines().map { line ->
+    val factories = File("day19input").readLines().map { line ->
         val match = blueprintRegex.matchEntire(line)!!
         Blueprint(match.groups[1]!!.value.toInt(),
             match.groups[2]!!.value.toInt(),
@@ -18,13 +19,27 @@ fun main(args: Array<String>) {
 
     println("There are ${factories.size} factories")
 
+    // Part 1 - calculate the quality level of all the blueprints
     val startTime = System.currentTimeMillis()
-    val result = factories[0].produceMax(ResourceType.Geode, 24, hashMapOf(Pair(ResourceType.None, 0)), hashMapOf(Pair(ResourceType.Ore, 1)))
-    //val result = factories.sumOf { it.id * it.produceMax(ResourceType.Geode, 24, HashMap<ResourceType, Int>(), hashMapOf(Pair(ResourceType.Ore, 1)))  }
+    val part1 = factories.sumOf { it.id * it.produceMax(ResourceType.Geode, 24, HashMap<ResourceType, Int>(), hashMapOf(Pair(ResourceType.Ore, 1)))  }
     val totalTime = System.currentTimeMillis() - startTime
-    println("Mined $result resources in $totalTime ms")
-    //println("Total quality level is $result; found in $totalTime ms")
+    println("Total quality level is $part1; found in $totalTime ms")
+
+    // Part 2 - calculate the product of the max geode produced by the first three blueprints
+    val part2 = factories.take(3).fold(1) { acc, factory ->
+        acc * factory.produceMax(
+            ResourceType.Geode,
+            32,
+            hashMapOf(Pair(ResourceType.None, 0)),
+            hashMapOf(Pair(ResourceType.Ore, 1))
+        )
+    }
+    println("Product of max geodes produced from the first three blueprints is $part2")
+
 }
+
+fun maxGeode(timeRemaining: Int, currentGeode: Int, currentRobots: Int) =
+    (timeRemaining * (timeRemaining + 1) / 2) + (currentRobots * timeRemaining) + currentGeode
 
 enum class ResourceType { Ore, Clay, Obsidian, Geode, None }
 
@@ -58,36 +73,34 @@ class Factory(blueprint: Blueprint) {
         Pair(ResourceType.Geode, Int.MAX_VALUE - 1000),
         Pair(ResourceType.None, Int.MAX_VALUE - 1000),
     )
+    var bestSolution = 0
 
     fun produceMax(resourceType: ResourceType,
                    time: Int,
                    resources: HashMap<ResourceType, Int>,
                    robots: HashMap<ResourceType, Int>): Int {
-//        println("Time Remaining: $time")
-//        resources.forEach { (type, count) ->
-//            println("$type: $count")
-//        }
-//        robots.forEach { (type, count) ->
-//            println("$type robots: $count")
-//        }
+        // If there is no hope of beating the best known solution, bail out now
+        if (maxGeode(time, resources.getOrDefault(ResourceType.Geode, 0), robots.getOrDefault(ResourceType.Geode, 0)) < bestSolution)
+            return 0
 
         if (time > 1) {
             // Filter out which robots to build based on the max needed based on the blueprint
-            //var canBuild = ResourceType.values().toList()
             var canBuild = ResourceType.values().filter { robots.getOrDefault(it, 0) < maxNeeded.getValue(it) }
-            canBuild = canBuild.filter { resources.getOrDefault(it, 0) < maxNeeded.getValue(it) }
+            // Filter out based on having a large stockpile of a resource (unproven heuristic)
+            canBuild = canBuild.filter { resources.getOrDefault(it, 0) < maxNeeded.getValue(it) + 3 }
             // Filter out robots that cost too much
             canBuild = canBuild.filter { type ->
                 robotCosts.getValue(type).all {
                     resources.getOrDefault(it.first, 0) >= it.second
                 }
             }.toList()
+            // If there are only two intervals remaining, only consider building a robot for the desired resource
+            if (time == 2)
+                canBuild = canBuild.filter { it == resourceType || it == ResourceType.None }
             // Produce the resources for this interval
             robots.forEach { (type, count) ->
                 resources[type] = resources.getOrDefault(type, 0) + count
             }
-            // If there are only two intervals remaining, only consider building a robot for the desired resource
-            if (time == 2) canBuild = canBuild.filter { it == resourceType || it == ResourceType.None }
             // Make the recursive call to get the maximum resourceType generated from each choice
             return canBuild.map { type ->
                 val newResources = HashMap(resources)
@@ -103,7 +116,9 @@ class Factory(blueprint: Blueprint) {
             robots.forEach { (type, count) ->
                 resources[type] = resources.getOrDefault(type, 0) + count
             }
-            return resources.getOrDefault(resourceType, 0)
+            val result = resources.getOrDefault(resourceType, 0)
+            bestSolution = max(bestSolution, result)
+            return result
         }
     }
 }
